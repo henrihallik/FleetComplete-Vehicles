1b877b4f9c334949a3877a5b000da7c441824c53
Henri Hallik <henri.ha@gmail.com>
2020-12-13 14:13:12 +0200

    Converted to kotlin class

---
+++ app/src/main/java/com/fleetcomplete/vehicles/view/TouchInterceptFrameLayout.kt
@@ -0,0 +1,39 @@
+package com.fleetcomplete.vehicles.view
+
+import android.annotation.TargetApi
+import android.content.Context
+import android.os.Build
+import android.util.AttributeSet
+import android.view.GestureDetector
+import android.view.MotionEvent
+import android.widget.FrameLayout
+
+class TouchInterceptFrameLayout : FrameLayout {
+    private var detector: GestureDetector? = null
+
+    constructor(context: Context?) : super(context!!) {}
+    constructor(context: Context?, attrs: AttributeSet?) : super(context!!, attrs) {}
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int) : super(context!!, attrs, defStyleAttr) {}
+
+    @TargetApi(Build.VERSION_CODES.LOLLIPOP)
+    constructor(context: Context?, attrs: AttributeSet?, defStyleAttr: Int, defStyleRes: Int) : super(context!!, attrs, defStyleAttr, defStyleRes) {
+    }
+
+    override fun onInterceptTouchEvent(ev: MotionEvent): Boolean {
+        if (detector != null) {
+            detector!!.onTouchEvent(ev)
+        }
+        return super.onInterceptTouchEvent(ev)
+    }
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        if (detector != null) {
+            detector!!.onTouchEvent(event)
+        }
+        return super.onTouchEvent(event)
+    }
+
+    fun setDetector(detector: GestureDetector?) {
+        this.detector = detector
+    }
+}
